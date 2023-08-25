ba9263939eb375512409510113a3f6ad3802c960
s3927562 <s3927562@rmit.edu.vn>
2023-08-25 16:31:21 +0700

    Start

--- Battleship/ContentView.swift
+++ Battleship/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) private var colorScheme // Get system color scheme
+    @AppStorage("firstLaunch") private var firstLaunch = true // Set first launch status
+    @AppStorage("darkMode") private var darkMode = false // Store dark mode settings
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                
+            } label: {
+                Text("New Game")
+                    .frame(maxWidth: .infinity, maxHeight: 32)
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button {
+                
+            } label: {
+                Text("Continue")
+                    .frame(maxWidth: .infinity, maxHeight: 32)
+            }
+            .buttonStyle(.bordered)
+            .disabled(true)
+            
+            Button {
+                
+            } label: {
+                Text("Leaderboard")
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Settings")
+            }
+            
+            Button {
+                
+            } label: {
+                Text("How to Play")
+            }
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "info.circle.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(Color.white, Color.accentColor)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
         }
         .padding()
+        .onAppear() {
+            // Set color scheme state variable on first launch based on system settings
+            if firstLaunch {
+                darkMode = colorScheme == .dark ? true : false
+                firstLaunch = false
+            }
+        }
+        .preferredColorScheme(darkMode ? .dark : .light) // Set color scheme based on state variable
     }
 }
 
